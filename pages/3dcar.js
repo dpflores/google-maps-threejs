525626037aa2892e5d5e77e5541eeeddbcbfdc38
Del Piero <delpiero22.flores@gmail.com>
2024-06-05 23:41:01 -0500

    u

--- pages/3dcar.js
+++ pages/3dcar.js
@@ -1,30 +1,41 @@
 import { Canvas } from "@react-three/fiber";
-import { Environment, OrbitControls } from "@react-three/drei";
-import { Suspense, useEffect, useState } from "react";
+import { useLoader } from "@react-three/fiber";
+import { useGLTF, Stage, PresentationControls } from "@react-three/drei";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { Suspense } from "react";
+
+const Model = () => {
+  const gltf = useLoader(GLTFLoader, "/tesla_model_3/scene.gltf");
+  console.log(gltf);
+  let group = gltf.scene;
+  return (
+    <>
+      <primitive object={gltf.scene} scale={0.4} />
+    </>
+  );
+};
 
 export default function App() {
-  const [model, setModel] = useState(null);
-  useEffect(() => {
-    loadModel().then((model) => {
-      //   setModel(model);
-    });
-  }, []);
-
   return (
-    <Canvas>
-      <ambientLight />
-      <OrbitControls enableZoom={false} />
-      <Suspense fallback={null}>{model}</Suspense>
-      <Environment preset="sunset" />
-    </Canvas>
+    <div className="App">
+      <Canvas
+        dpr={[1, 2]}
+        shadows
+        camera={{ fov: 45 }}
+        style={{ position: "absolute" }}
+      >
+        {/* <color attach="background" args={["#101010"]} /> */}
+        <PresentationControls
+          speed={1.5}
+          global
+          zoom={0.5}
+          polar={[-0.1, Math.PI / 4]}
+        >
+          <Stage environment={"sunset"}>
+            <Model scale={0.01} />
+          </Stage>
+        </PresentationControls>
+      </Canvas>
+    </div>
   );
-}
-
-async function loadModel() {
-  const loader = new GLTFLoader();
-  const object = await loader.loadAsync("/tesla_model_3/scene.gltf");
-  const group = object.scene;
-  group.scale.setScalar(0.2);
-  return group;
 }
